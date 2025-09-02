90bb788a907482c627b8ed80eb46f50804895d62
JK <laumelanie249@gmail.com>
2025-09-02 12:08:51 -0500

    subiendo ejercicio 1

---
+++ fundamentos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fundamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+
+        static void ejer1()
+        {
+            string nom, carrera;
+            Console.Write("Ingrese su nombre: ");
+            nom = Console.ReadLine();
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"{nom}, bienvenida al curso de Fundamentos de Algoritmos de la carrera {carrera}");
+        }
+
+
+        static void ejer2()
+        {
+
+        }
+
+        static void ejer3()
+        {
+
+        }
+
+        static void ejer4()
+        {
+
+        }
+
+        static void ejer5()
+        {
+
+        }
+
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ fundamentos_py/fundamentos_py.py
@@ -0,0 +1,6 @@
+﻿def ejer1():
+    nom=input("Ingrese su nombre: ")
+    carrera=input("Ingrese su carrera: ")
+    print(f"{nom}, bienvenida al curso de Fundamentos de Algoritmos de la carrera {carrera}")
+           
+ejer1()
